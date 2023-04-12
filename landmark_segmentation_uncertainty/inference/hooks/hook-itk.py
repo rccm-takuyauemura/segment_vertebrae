dcd0a579d2c61b7b45ccf2d2dbce5c0ba87fd1b3
Takuya Uemura <130040469+rccm-takuyauemura@users.noreply.github.com>
2023-04-12 17:14:06 +0900

    modify hookfile & requirement file

--- landmark_segmentation_uncertainty/inference/hooks/hook-itk.py
+++ landmark_segmentation_uncertainty/inference/hooks/hook-itk.py
@@ -6,10 +6,7 @@ from PyInstaller.utils.hooks import collect_data_files
 itk_datas = collect_data_files('itk', include_py_files=True)
 datas = [x for x in itk_datas if '__pycache__' not in x[0]]
 
-
-from PyInstaller.utils.hooks import collect_submodules
-
 hiddenimports = [
-        *collect_submodules('skimage'),
-        *collect_submodules('cachetools'),
+        'skimage',
+        'cachetools',
         ]
